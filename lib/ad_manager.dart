df05def07dc34353188f4d5e59fbbd45e297cf79
Samuel3Shin <samuel3.shin@gmail.com>
2021-04-03 15:58:32 +0900

    added firebase admob configs

---
+++ lib/ad_manager.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+class AdManager {
+  static String get appId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544~4354546703";
+      // return "ca-app-pub-2551410065860924~3796958759";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544~2594085930";
+      // return "ca-app-pub-2551410065860924~3597648201";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/8865242552";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544/4339318960";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get interstitialAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/7049598008";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544/3964253750";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+
+  static String get rewardedAdUnitId {
+    if (Platform.isAndroid) {
+      return "ca-app-pub-3940256099942544/8673189370";
+    } else if (Platform.isIOS) {
+      return "ca-app-pub-3940256099942544/7552160883";
+    } else {
+      throw new UnsupportedError("Unsupported platform");
+    }
+  }
+}
